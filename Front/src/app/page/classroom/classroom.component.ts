6dd05a00b4c85e5adfcac52fd842cebff3bbb5f7
teeser07 <nathee200@gmail.com>
2021-07-05 22:55:12 +0700

    [modal]

--- Front/src/app/page/classroom/classroom.component.ts
+++ Front/src/app/page/classroom/classroom.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup ,FormBuilder } from '@angular/forms';
+import { ClassroomService } from './classroom.service';
+import { debounceTime } from 'rxjs/operators';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-classroom',
@@ -6,10 +10,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./classroom.component.scss']
 })
 export class ClassroomComponent implements OnInit {
+  searchControl: FormControl = new FormControl();
+  products;
+  filteredProducts;
+  confirmResut;
+  addRoomForm : FormGroup ;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(
+    private classroomService : ClassroomService,
+    private modalService: NgbModal,
+    private FormBuilder: FormBuilder
+   ) { 
+    this.addRoomForm = this.FormBuilder.group([
+      
+    ])
+
+    
   }
 
+  ngOnInit() {
+    this.classroomService.getRoom()
+    .subscribe((res: any[]) => {
+      this.products = [...res];
+      this.filteredProducts = res;
+    });
+
+    this.searchControl.valueChanges
+    .pipe(debounceTime(200))
+    .subscribe(value => {
+      this.filerData(value);
+    });
+  }
+  
+  filerData(val) {
+    if (val) {
+      val = val.toLowerCase();
+    } else {
+      return this.filteredProducts = [...this.products];
+    }
+
+    const columns = Object.keys(this.products[0]);
+    if (!columns.length) {
+      return;
+    }
+
+    const rows = this.products.filter(function(d) {
+      for (let i = 0; i <= columns.length; i++) {
+        const column = columns[i];
+        // console.log(d[column]);
+        if (d[column] && d[column].toString().toLowerCase().indexOf(val) > -1) {
+          return true;
+        }
+      }
+    });
+    this.filteredProducts = rows;
+  }
+  
+  confirm(content) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title'})
+    .result.then((result) => {
+      console.log(result);
+    }, (reason) => {
+      console.log('Err!', reason);
+    });
+  }
+
+  addRoom() {
+    this.classroomService.addRoom
+  }
 }
--- Front/src/app/page/classroom/classroom.service.ts
+++ Front/src/app/page/classroom/classroom.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,8 +10,14 @@ export class ClassroomService {
 
   constructor(private http: HttpClient) { }
 
-  getUser() {
-    return this.http.get('account/user');
+  getRoom() {
+    return this.http.get('Room/get-room');
   }
-  
+  addRoom(data) {
+    return this.http.post<any>('Room/save-room',{data});
+  }
+  deleteRoom(id) {
+    return this.http.disableApiPrefix().delete<any[]>('Room/delete-room'+id);
+}
+
 }
