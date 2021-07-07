19a98ad6d8c098be8d47bc62a2a327191585ea88
teeser07 <nathee200@gmail.com>
2021-07-07 15:22:29 +0700

    [day]

--- Front/src/app/page/day/day.service.ts
+++ Front/src/app/page/day/day.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DayService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getDay(){
+    return this.http.get('Days/get-days');
+  }
+
+  deleteDay(id:any){
+    return this.http.delete(`Days/delete-days?days_id=${id}`);
+  }
+
+  saveDay(data:any){
+    return this.http.post('Days/save-days',data)
+  }
+
+  getDayDetail(id:any){
+    return this.http.get(`Days/get-days-detail?days_id=${id}`)
+  }
+
+  updateDay(id:any,data:any){
+    return this.http.put(`Days/update-days?days_id=${id}`,data)
+  }
 }
